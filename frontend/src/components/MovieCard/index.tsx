de57878b1550aee3991aeca1725209ea7c20d54f
Carlos <mugetsuvita@gmail.com>
2022-04-07 21:16:01 -0300

    whatsapp share and form css edits

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -12,7 +12,7 @@ function MovieCard( { movie } : Props){
             <img className="image-movie-global" src={movie.image} alt={movie.name} />
             <div className="movie-card-2nd-container">
                 <div className="movie-card-3rd-container">
-                <h5 id="movieName">{movie.name}</h5>
+                <h5 id="movieCardName">{movie.name}</h5>
                 </div>
                 <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary" id="assistirButton">Assistir</div>
