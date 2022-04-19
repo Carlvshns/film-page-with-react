2500bf9aaa2c5db93d9409f5f51bad1e7e8bdbc4
Carlos <mugetsuvita@gmail.com>
2022-04-19 16:44:07 -0300

    slide responsave finish

--- frontend/src/components/MiniMovieCard/index.tsx
+++ frontend/src/components/MiniMovieCard/index.tsx
@@ -14,10 +14,12 @@ function MiniMovieCard( { movie } : Props){
             </Link>
         <div className="mini-movie-card-2nd-container">
             <div className="mini-movie-card-3rd-container">
-            <h6 id="mini-movieCardName">{movie.name}</h6>
+            <p id="mini-movieCardName">{movie.name}</p>
             </div>
             <Link to={`/form/${movie.id}`} style={{ textDecoration: 'none'}}>
-                <div className="btn btn-primary" id="mini-assistirButton">Assistir</div>
+                <div className="btn btn-primary" id="mini-assistirButton">Assistir&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-collection-play-fill" viewBox="0 0 16 16">
+                    <path d="M2.5 3.5a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-11zm2-2a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6v7zm6.258-6.437a.5.5 0 0 1 .507.013l4 2.5a.5.5 0 0 1 0 .848l-4 2.5A.5.5 0 0 1 6 12V7a.5.5 0 0 1 .258-.437z"/>
+                    </svg></div>
             </Link>
         </div>
     </div>
