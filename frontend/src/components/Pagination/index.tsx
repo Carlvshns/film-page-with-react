fc4a97268915b79fcdfbc4bd7a29100aecd881e0
Carlos <mugetsuvita@gmail.com>
2022-06-17 11:38:22 +0600

    removes names edits

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -9,13 +9,13 @@ type Props = {
 
 function Pagination( {page, onChange} : Props){
     return(
-        <div className="dsmovie-pagination-container">
-            <div className="dsmovie-pagination-box">
-                <button className="dsmovie-pagination-button" disabled={page.first} onClick={() => onChange(page.number - 1)}>
-                    <Arrow className="dsmovie-flip-horizontal" />
+        <div className="pagination-container">
+            <div className="pagination-box">
+                <button className="pagination-button" disabled={page.first} onClick={() => onChange(page.number - 1)}>
+                    <Arrow className="flip-horizontal" />
                 </button>
                 <p id="pageNumbers">{`${page.number + 1} de ${page.totalPages}`}</p>
-                <button className="dsmovie-pagination-button" disabled={page.last} onClick={() => onChange(page.number + 1)}>
+                <button className="pagination-button" disabled={page.last} onClick={() => onChange(page.number + 1)}>
                     <Arrow />
                 </button>
             </div>
